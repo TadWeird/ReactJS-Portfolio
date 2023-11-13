4b72f52d5066b7c1be8712f0244dc2fa90cc99ad
PlopN <jrmaliwat0424@gmail.com>
2023-11-13 22:17:52 +0800

    Trial and Error

--- ReactJS-Portfolio/src/App.jsx
+++ ReactJS-Portfolio/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navvy from './Components/Navvy'
 
 function App() {
   
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Suck My Nut
-    </h1>
+    <div>
+        <Navvy/>
+    </div>
+
   )
 }
 
---
+++ ReactJS-Portfolio/src/Components/Navvy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navvy() {
+  return (
+    <div className='text-purple-400 font-bold text-6xl'>
+      Hello
+    </div>
+  )
+}
+
+export default Navvy
