f1c288cbae97d0223d701b3a1e6fb502582710c0
PlopN <jrmaliwat0424@gmail.com>
2023-11-18 02:22:58 +0800

    Change

--- ReactJS-Portfolio/src/Components/Heero.jsx
+++ ReactJS-Portfolio/src/Components/Heero.jsx
@@ -5,7 +5,7 @@ function Heero() {
   return (
 
     <section class="bg-gradient-to-r from-rose-100 to-teal-100">
-    <div class="container mx-auto flex md:px-24 md:py-32  md:flex-row flex-col items-center">
+    <div class="container mx-auto flex md:px-24 md:py-52  md:flex-row flex-col items-center">
         <div
             class="lg:flex-grow mt-5 md:mt-0 md:w-1.5/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1
