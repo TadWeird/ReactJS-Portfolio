8ca897ee2a5662919405f0474c58237e67c5e5f5
PlopN <jrmaliwat0424@gmail.com>
2023-11-22 21:28:43 +0800

    Revision For SnT

--- src/Components/SnT.jsx
+++ src/Components/SnT.jsx
@@ -11,7 +11,7 @@ export default function SnT() {
         <p class=" pt-1 pb-10 text-3xl text-gray-700 text-center"> "Here Are Some Of The Languages And Platforms I am Currently Learning For My Career" </p>
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-3 gap-5 md:px-32  md:w-10/12">
+    <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 md:px-32 w-9/12 md:w-11/12">
 
         <div class="bg-white p-4 rounded-lg shadow-md transform transition-transform hover:scale-105">
             <img class="w-full h-auto object-cover mb-2"
